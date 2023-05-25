ecd4050462fe2d8c5607e1683882cee02d6ef0d4
小白·菜 <58581661+infinityglow@users.noreply.github.com>
2023-05-26 00:11:53 +0800

    Delete Assignments/Assignment 2 directory

--- Assignments/Assignment 2/src/SlideListenerClient.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
-public class SlideListenerClient implements ChangeListener {
-    @Override
-    public void stateChanged(ChangeEvent e) {
-        DrawListenerClient.stroke = (float) ClientGUI.js.getValue() / 10;
-    }
-}
